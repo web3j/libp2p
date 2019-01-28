9fbfd3a9d5df4c696ca9eb9be5800577ba88f502
Sam Nazha <sam@blk.io>
2019-01-28 20:30:47 +1100

    Cleanup

--- libp2p-peer/src/test/kotlin/io/web3j/libp2p/peer/MultihashLibTest.kt
+++ libp2p-peer/src/test/kotlin/io/web3j/libp2p/peer/MultihashLibTest.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2019 BLK Technologies Limited (web3labs.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package io.web3j.libp2p.peer
 
 import io.ipfs.multiformats.multihash.Type
@@ -6,7 +18,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.security.MessageDigest
-import java.util.*
+import java.util.Base64
 import io.ipfs.multiformats.multihash.Multihash as KMultihash
 import io.ipfs.multihash.Multihash as JMultihash
 
@@ -39,7 +51,6 @@ class MultihashLibTest {
     JGAzKpyttXBTIDO51AkEA98KTiROMnnU8Y6Mgcvr68/SMIsvCYMt9/mtwSBGgl80VaTQ5Hpaktl6Xbh
     VUt5Wv0tRxlXZiViCGCD1EtrrwTw==
     """.trimIndent().replace("\n", "")
-
 
     @Test
     fun loadUsingJavaLibrary() {
@@ -81,5 +92,4 @@ class MultihashLibTest {
             "Kotlin-library multi-hash values differ for the public key"
         )
     }
-
 }
